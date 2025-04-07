b46f4a0582533a358d154172cc1d48df70d56afe
Chahat1804 <122960416+Chahat1804@users.noreply.github.com>
2025-04-07 14:28:36 +0530

    commit done

--- views/app.jsx
+++ views/app.jsx
@@ -7,8 +7,13 @@ import BestsellersSection from './components/BestsellersSection';
 import WelcomeSection from './components/WelcomeSection';
 import Footer from './components/Footer';
 import CategoryPage from './pages/CategoryPage';
+import WomenPage from './pages/WomenPage';
 import Login from './pages/login'; // Add this import
 import Signup from "./pages/Signup";
+import WomenProductsPage from './pages/WomenProductsPage';
+import LookbookSection from './components/LookbookSection';
+import WomenHeroSection from './components/WomenHeroSection';
+import RecentProducts from './components/RecentProducts';
 const App = () => {
   return (
     <div
@@ -31,12 +36,17 @@ const App = () => {
                 <SaleSection />
                 <BestsellersSection />
                 <WelcomeSection />
+                {/* <LookbookSection />
+                <WomenHeroSection />
+                <RecentProducts /> */}
               </>
             }
           />
           <Route path="/shop/:categoryName" element={<CategoryPage />} />
           <Route path="/login" element={<Login />} /> {/* Add this new route */}
           <Route path="/signup" element={<Signup />} />
+          <Route path="/women" element={<WomenPage />} />
+          <Route path="/women-product" element={<WomenProductsPage />} />
         </Routes>
       </main>
       <Footer />
@@ -44,4 +54,4 @@ const App = () => {
   );
 };
 
-export default CategoryPage;
+export default App;
